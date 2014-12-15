a97fdb4b7db49c40d9a92ad3c4b62a571e52f079
HsChuBin <hschubin@foxmail.com>
2014-12-16 07:41:49 +0800

    fix the method of scrolTo

--- active.js
+++ active.js
@@ -1,23 +1,54 @@
 $(document).ready(function() {
 	$("#nav div").mouseover(function() {
 		$(this).next().css({"color":"#B7B8AD"});
-
-		var target;	//目标位置
-		switch($(this).parent().attr("id")) {
-			case "di1": target = 0; break;
-			case "di2": target = 400; break;
-			case "di3": target = 800; break;
-			case "di4": target = 1200; break;
-			case "di5": target = 1600; break;
-			default: target = 0;
-		}
-		if(scrol) {
-			clearInterval(scrol);
-		}
-		scrol = setInterval(scrolTo, 1, target);//移动滚动条函数
-
+		$(this).toTarget();
 	}).mouseout(function() {
 		$(this).next().css({"color":"#abcdef"});
+	});
+
+	var judge = true;	//判断是否停止动画
+	$("#skills li").mouseover(function() {
+		if(setTime) {
+			clearTimeout(setTime);
+		}
+		if(judge) {
+			$(this).css("background-color", "#fff");
+			$(this).showExplanation();
+		}
+	}).mouseout(function() {
+		if(judge) {
+			$(this).hideExplanation();
+		}
+	}).click(function() {
+		$(this).css("background-color", "#fff")
+			.siblings().css("background-color", "#abcdef");
+		judge = false;
+		var text = $(this).text();
+		for(var i=0; i < $(".skill").length; i++) {
+			if($(".skill")[i].getAttribute("title") == text) {
+				$($(".skill")[i]).fadeIn("slow")
+					.siblings().css("display", "none");
+			}
+		}
+	});
+
+	$("#explanation li").mouseover(function() {
+		if(setTime) {
+			clearTimeout(setTime);
+		}
+		if(judge) {
+			$(this).showExplanation();
+		}
+		for(var i=0; i<$("#skills li").length; i++) {
+			var title = $(this).attr("title");
+			if($("#skills li")[i].innerHTML == title) {
+				$($("#skills li")[i]).css("background-color", "#fff");
+			}
+		}
+	}).mouseout(function() {
+		if(judge) {
+			$(this).hideExplanation();
+		}
 	});
 });
 
@@ -25,7 +56,7 @@ $(document).ready(function() {
 var scrol;	//导航的定位计时器
 
 function scrolTo(target) {
-	var step = 12;
+	var step = 11;
 	var local = $(window).scrollTop();
 	var distance;
 	target > local ?
@@ -34,8 +65,47 @@ function scrolTo(target) {
 
 	if(local == target) {
 		clearInterval(scrol);
+	} else if(distance < 2 && distance > -2) {
+		$(window).scrollTop(target);
+		clearInterval(scrol);
 	} else {
 		local += distance;
 		$(window).scrollTop(local);
 	}
 }//将滚动条拖曳至指定位置的动画
+
+$.fn.showExplanation = function() {
+	$("#explanation").slideDown();
+	var text = $(this).text();
+	for(var i=0; i < $(".skill").length; i++) {
+		if($(".skill")[i].getAttribute("title") == text) {
+			$($(".skill")[i]).fadeIn("slow")
+				.siblings().css("display", "none");
+		}
+	}
+}
+var setTime;	//个人能力说明收起计时器
+$.fn.hideExplanation = function() {
+	$("#skills li").css("background-color", "#abcdef");
+	setTime = setTimeout(function() {
+		$(".skill").css("display", "none");
+		$('#explanation').slideUp();
+	}, 600);
+}
+
+$.fn.toTarget =function() {
+	var target = 0;
+	switch($(this).parent().attr("id")) {
+		case "di1": target = 0; break;
+		case "di2": target = 400; break;
+		case "di3": target = 800; break;
+		case "di4": target = 1200; break;
+		case "di5": target = 1600; break;
+		default: target = 0;
+	}
+
+	if(scrol) {
+		clearInterval(scrol);
+	}
+	scrol = setInterval(scrolTo, 1, target);//移动滚动条函数
+}
